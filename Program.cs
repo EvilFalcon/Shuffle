2b9b9bd6abe7abf6f78aeafa8e67a083c1b0112a
EvilFalcon <ares.sokol@yandex.ru>
2023-01-26 15:06:24 +0300

    Shuffle_1.0

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Shuffle
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            Console.WriteLine(string.Join(" ", array));
+
+            ShuffleArray(array);
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+        private static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    int randomIndex = random.Next(array.Length);
+
+                    (array[j], array[randomIndex]) = (array[randomIndex], array[j]);
+                }
+            }
+        }
+
+    }
+}
